f12446adf36837b66b50adb4c511f345d1d699e7
lubien <lubien1996@gmail.com>
2019-05-04 16:47:06 -0300

    Separate functions bresenham.js to utils.js

--- src/algorithms/utils.js
+++ src/algorithms/utils.js
@@ -43,3 +43,46 @@ export default class Utils {
     array[idx] = newItem
   }
 }
+
+export function negate (x) {
+  return -x
+}
+
+export function reflection (p1, p2) {
+  const slope = lineSlope(p1, p2)
+  const swapXY = Math.abs(slope) > 1
+
+  const [x1, y1] = swapXY ? p1.reverse() : p1
+  const [x2, y2] = swapXY ? p2.reverse() : p2
+
+  const swapX = x1 > x2
+  const swapY = y1 > y2
+
+  const [nextX1, nextX2] = swapX ? [x1, x2].map(negate) : [x1, x2]
+  const [nextY1, nextY2] = swapY ? [y1, y2].map(negate) : [y1, y2]
+
+  const nextP1 = [nextX1, nextY1]
+  const nextP2 = [nextX2, nextY2]
+
+  return [nextP1, nextP2, swapXY]
+}
+
+export function reverseReflection (pixels, swapXY) {
+  return pixels.map(pixel => {
+    const [x, y] = swapXY ? pixel.reverse() : pixel
+    const nextX = Math.abs(x)
+    const nextY = Math.abs(y)
+    return [nextX, nextY]
+  })
+}
+
+export function lineSlope ([x1, y1], [x2, y2]) {
+  const dx = x2 - x1
+  const dy = y2 - y1
+
+  if (dx === 0) {
+    return dy
+  }
+
+  return dy / dx
+}
